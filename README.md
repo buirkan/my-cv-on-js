fdfd1225790f911d6b6e6b2d6e545ae069b5f205
Felipe Silva <felipealvessilva64@gmail.com>
2020-05-15 17:40:05 -0300

    Creating some informations 🧾

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# my-cv-on-js
+# My Curriculum on Js
 A simple script with my curriculum on JavaScript format 🐢
+
+---
+### My Resume
+![My informations data](img\curriculumImage.png)
---
+++ curriculum.js
@@ -0,0 +1,71 @@
+'use strict'
+
+/** */
+function myCurriculum() {
+    const name = 'Felipe Alves da Silva'
+    const age = 22
+    const objectives = ['Front-end Engineer', 'Fullstack Engineer']
+    const technologies = {
+        languages: [
+            'JavaScript',
+            'Typescript',
+            'Python',
+            'C#',
+            'Java'
+        ],
+        libraries: [
+            'ReactJs Ecosystem',
+            'Chart.Js',
+            'Webpack'
+        ],
+        frameworks: [
+            'Angular',
+            'GraphQL',
+            'Jest',
+            'Apollo GraphQL',
+            'Redux',
+            'React Testing Library',
+            'Enzyme'
+        ]
+    }
+    const habilities = []
+    const knowledges = [
+        'React Hooks',
+        'GIT',
+        'MVC',
+        'es6',
+        'Scrum',
+        'REST',
+        'Microservices',
+        'Docker',
+        'TDD'
+    ]
+    const courses = {
+        finished: [
+            {
+                course: 'Ensinamentos avançados em JavaScript - OO com es6',
+                platform: 'Alura Cursos'
+            },
+            {
+                course: 'Testing React Applications using Jest framework',
+                platform: 'Pluralsight'
+            },
+            {
+                course: 'Omnistack week 11.0 - (NodeJs, ReactJs and React Native)',
+                platform: 'Rocketseat'
+            }
+        ],
+        inProgress: [
+            {
+                course: 'Microservices with Node JS and React',
+                platform: 'Udemy Courses'
+            },
+            {
+                course: 'Docker and Kubernetes: The Complete Guide',
+                platform: 'Udemy Courses'
+            }
+        ]
+    }
+    const resume = ''
+}
+module.exports = myCurriculum
